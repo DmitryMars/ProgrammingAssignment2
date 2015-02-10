d0bd334c5543e8d959d3dfb94e999d33a57ae154
DmitryMars <dmitry.mm@icloud.com>
2015-02-11 00:17:00 +0500

    assignment is done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve functions allows to calculate inverse matrix
+## for given matrix x and cache inverse in special matrix object.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates and returns a wrapper for matrix x. It stores 
+## cache of inverse matrix for the matrix x. To calculate and cache inverse 
+## matrix of x use cacheSolve function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    
+    set <- function(y) {
+        x <<- y # variables x and inv are closed inside makeCacheMatrix scope  
+        inv <<- NULL 
+    }
+    
+    get <- function() x
+    
+    setinverse <- function(inverse) inv <<- inverse
+    
+    getinverse <- function() inv
+    
+    return(list(set = set, 
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse))
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calculates, caches and returns inverse for matrix x. 
+## x shold be a matrix wrapper, created by makeCacheMatrix function.
+## An exceptions occurs if: a) x is nott a matrix wrapper object
+##                          b) matrix is not invertible (not square  
+## or near-singular)                           
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inversecache<-x$getinverse()
+    
+    if(!is.null(inversecache)){
+        message("retrieving inverse from cache...")
+        return(inversecache)
+    }
+    
+    inverse<-solve(x$get())
+    x$setinverse(inverse, ...)
+    
+    ## Return a matrix that is the inverse of 'x'
+    return(inverse)
 }
